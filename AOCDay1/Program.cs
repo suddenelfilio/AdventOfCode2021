9d700b114e6258640c54a5c09f2ed1a350326e93
Filip Stas <filip@visuasoft.be>
2021-12-03 19:21:53 +0100

    day 1 part 2

--- AOCDay1/Program.cs
+++ AOCDay1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace AOCDay1
 {
@@ -6,23 +7,37 @@ namespace AOCDay1
     {
         static void Main(string[] args)
         {
-            var fp = new FileParser<IncrementCounter>(@"D:\Playground\AdventOfCode\AOCDay1\input.txt");
-            fp.Parsers.Add(new LineParser<IncrementCounter>((string s, IncrementCounter instance) =>
+            var fp = new FileParser<SlidingCounter>(@"/Users/filip/Documents/Playground/AdventOfCode2021/AOCDay1/input.txt");
+            fp.Parsers.Add(new LineParser<SlidingCounter>((string s, SlidingCounter instance) =>
             {
                 if (int.TryParse(s, out int i))
                 {
-                    if (instance.PreviousValue.HasValue && i > instance.PreviousValue)
-                    {
-                        instance.NumberOfIncrements+=1;
-                    }
-                    instance.PreviousValue = i;
-
+                    instance.Values.Add(i);
                 }
                 return instance;
             }));
 
-            Console.WriteLine(fp.Parse().NumberOfIncrements);
+            int[] arr = fp.Parse().Values.ToArray();
+            System.Console.WriteLine(GetIncrements(arr, arr.Length, 3));
             Console.ReadLine();
+        }
+
+        private static int GetIncrements(int[] arr, int count, int slideSize)
+        {
+            int col = 0, cnt = 0;
+            while (col < count - slideSize)
+            {
+                int s1 = 0, s2 = 0;
+                for (int i = 0; i < slideSize; i++)
+                {
+                    s1 += arr[col + i];
+                    s2 += arr[col + i + 1];
+                }
+                if (s1 < s2)
+                    cnt++;
+                col++;
+            }
+            return cnt;
         }
     }
 }
---
+++ AOCDay1/SlidingCounter.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace AOCDay1
+{
+    public class SlidingCounter{
+        public List<int> Values { get; set; } = new List<int>();
+    }
+}
